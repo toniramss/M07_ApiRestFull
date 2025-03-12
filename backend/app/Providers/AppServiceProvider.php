390d07d10b48572617b1b4a577453dcc8bba49e8
toniramss <toni.rams.sebastia@gmail.com>
2025-03-12 10:22:53 +0100

    Ajustar maximo de peticiones de la api y cambiar estilo css general

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('api', function ($request) {
+            return Limit::perMinute(1000)->by(optional($request->user())->id ?: $request->ip());
+        });
     }
 }
